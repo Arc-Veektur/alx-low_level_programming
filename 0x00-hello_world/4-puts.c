d3ad90882cc65b7c93c1805d9a161b44a4aa0816
Veektur <voluoseh@gmail.com>
2023-02-16 15:42:29 -0800

    Task 4. Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - The use of function puts to print a line in programming
- * Return: 0
+ *
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (O);
+return (0);
 }
