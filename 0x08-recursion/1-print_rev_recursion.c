eee4b347ec96b2dcfbd748c9dd0b8410b6a137db
Veektur <voluoseh@gmail.com>
2023-03-08 13:25:34 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Entry point
+ * @s: input
+ * Return: Always 0 (success)
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+
+	}
+}
